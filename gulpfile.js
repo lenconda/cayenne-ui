49e371f3afcb6def134920a7e7f1bf8e6e217806
lenconda <i@lenconda.top>
2021-01-19 14:48:24 +0800

    feat: add circle-ci support

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,39 @@
-var gulp  = require('gulp'),
+const gulp = require('gulp'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
   cleanCss = require('gulp-clean-css'),
   rename = require('gulp-rename'),
-  postcss      = require('gulp-postcss'),
+  postcss = require('gulp-postcss'),
   autoprefixer = require('autoprefixer');
 
 function build() {
-    return gulp.src(['scss/*.scss'])
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([ autoprefixer({ browsers: [
-                'Chrome >= 35',
-                'Firefox >= 38',
-                'Edge >= 12',
-                'Explorer >= 10',
-                'iOS >= 8',
-                'Safari >= 8',
-                'Android 2.3',
-                'Android >= 4',
-                'Opera >= 12']})]))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css/'))
-        .pipe(cleanCss())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('css/'))
+  return gulp.src(['scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer({
+      browsers: [
+        'Chrome >= 35',
+        'Firefox >= 38',
+        'Edge >= 12',
+        'Explorer >= 10',
+        'iOS >= 8',
+        'Safari >= 8',
+        'Android 2.3',
+        'Android >= 4',
+        'Opera >= 12'
+      ]
+    })]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(cleanCss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/css/'))
 }
 
 function watcher() {
-    gulp.watch(['scss/*.scss'], gulp.series(build));
+  gulp.watch(['scss/*.scss'], gulp.series(build));
 }
 
 exports.watch = gulp.series(build, watcher);
